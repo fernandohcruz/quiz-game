cd920cced555d0584648ae1ba94a3d519198f6d1
Fernando Cruz <fernandocruz@algartelecom.com.br>
2022-10-05 17:29:05 -0300

    imported axios and started development

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+
+      <template v-for="answer in this.answers" :key="answer">
+        <input type="radio" name="options" :value="answer" v-model="this.chosen_answer">
+        <label v-html="answer"></label><br>
+      </template>
+
+      <button class="send" type="button" v-on:click="this.submitAnswer()">Send</button>
+    </template>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: [],
+      correctAnswer: undefined,
+      chosen_answer: undefined
+    }
+  },
+  methods: {
+    submitAnswer() {
+      if(!this.chosen_answer) {
+        alert('Pick one options');
+      } else {
+        if(this.chosen_answer == this.correctAnswer)
+          
+      }
+    }
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+
+      return answers;
+    }
+  },
+  created() {
+    this.axios.get('https://opentdb.com/api.php?amount=1&category=18').then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswer = response.data.results[0].correct_answer;
+
+    })
   }
 }
+
+// https://opentdb.com/api.php?amount=1&category=18
 </script>
 
 <style lang="scss">
@@ -21,6 +66,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
