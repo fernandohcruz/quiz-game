a027801706416cf8df5e1d392bf637d941676386
Fernando Cruz <fernandocruz@algartelecom.com.br>
2022-10-20 15:18:54 -0300

    criado outro projeto para estudos

---
+++ shopping_bag/src/store/index.js
@@ -0,0 +1,72 @@
+import { createStore } from 'vuex'
+import axios from 'axios';
+
+export default createStore({
+  state: {
+    products: [],
+    productsInBag: [],
+  },
+  mutations: {
+    loadProducts(state, products) {
+      state.products = products;
+    },
+
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
+
+    addToBag(state, product) {
+      state.productsInBag.push(product);
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
+    },
+
+    removeFromBag(state, productId) {
+      const index = state.productsInBag.findIndex(item => item.id == productId);
+      state.productsInBag.splice(index, 1);
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
+    },
+
+    decreaseQuantity(state, productId) {
+      const index = state.productsInBag.findIndex(item => item.id == productId);
+      state.productsInBag[index].quantity--;
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
+    },
+
+    increaseQuantity(state, productId) {
+      const index = state.productsInBag.findIndex(item => item.id == productId);
+      state.productsInBag[index].quantity++;
+      localStorage.setItem('productsInBag', JSON.stringify(state.productsInBag));
+    }
+  },
+  actions: {
+    loadProducts({ commit }) {
+      axios.get('https://fakestoreapi.com/products').then(response => {
+        commit('loadProducts', response.data);
+      })
+    },
+
+    loadBag({ commit }) {
+      if (localStorage.getItem('productsInBag'))
+        commit('loadBag', JSON.parse(localStorage.getItem('productsInBag')));
+    },
+
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
+    },
+
+    removeFromBag({ commit }, productId) {
+      if (confirm('Are you sure you want to remove the item from bag?'))
+        commit('removeFromBag', productId);
+    },
+
+    decreaseQuantity({ commit }, productId) {
+      commit('decreaseQuantity', productId);
+    },
+
+    increaseQuantity({ commit }, productId) {
+      commit('increaseQuantity', productId);
+    }
+  },
+  modules: {
+  }
+})
---
+++ shopping_bag/src/views/Product.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="test">
+        This is a product page TEST
+    </div>
+    <div class="test2">
+        <button type="button">I'm a button</button>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+.test {
+    width: 940px;
+    background-color: #000000;
+    font-size: 100px;
+    color: #ffffff;
+}
+
+.test2 {
+    button {
+        font-size: x-large;
+        width: 500px;
+        margin-top: 100px;
+        background-color: #43f4f4;
+        color: #ffffff;
+        transition: 1s;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #000000;
+        transform: rotate(360deg) scale(2);
+    }
+}
+</style>
